51efd810119996135040d0b7f6c539fb8bc1a8fe
Polina <polinakots0907@gmail.com>
2024-09-23 21:27:27 +0300

    what you did

--- Factory.cs
+++ Factory.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace test
 {
-    internal class Factory
+    public class Factory
     {
+        public int Id { get; set; }
+        public double P { get; set; }
+        public double F { get; set; }
+        public double O1 => P - F;
+        public double O2 => O1 * 100 / P;
+
+        public Factory(int id, double p, double f)
+        {
+            Id = id;
+            P = p;
+            F = f;
+        }
     }
 }
